a1884ce692e49949d38db1b992e524f405553d15
RodrigoJGF <rodrigoferreira.tico@gmail.com>
2025-05-16 00:42:45 -0300

    fix: CORS

--- src/main/java/dio/desafio/cloud/App.java
+++ src/main/java/dio/desafio/cloud/App.java
@@ -3,6 +3,10 @@ package dio.desafio.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server RL") })
 @SpringBootApplication
 public class App {
 
